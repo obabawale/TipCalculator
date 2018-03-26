e078a026323ae5856db3fc11412dd82e816d717c
obabawale <oebabawale@rocketmail.com>
2018-03-26 14:02:32 +0100

    Initial commit

---
+++ app/src/main/java/com/yahoo/olalekanbabawale/tipcalculator/MainActivity.java
@@ -0,0 +1,59 @@
+package com.yahoo.olalekanbabawale.tipcalculator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etBillAmount)
+    EditText etBillAmount;
+    @BindView(R.id.tvTipPercent)
+    TextView tvTipPercent;
+    @BindView(R.id.tvTipTotal)
+    TextView tvTipTotal;
+    @BindView(R.id.tvBillTotalAmount)
+    TextView tvBillTotalAmount;
+
+    float percentage = 0;
+    float tipTotal = 0;
+    float finalBillAmount = 0;
+
+    float REGULAR_TIP_PERCENTAGE = 10;
+    float DEFAULT_TIP_PERCENTAGE = 15;
+    float EXCELLENT_TIP_PERCENTAGE = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setTipValues();
+    }
+
+    private void setTipValues() {
+        tvTipPercent.setText(getString(R.string.main_msg_tippercent, percentage));
+        tvTipTotal.setText(getString(R.string.main_msg_tiptotal, tipTotal));
+        tvBillTotalAmount.setText(getString(R.string.main_msg_billtotalresult, finalBillAmount));
+    }
+
+    @OnClick({R.id.ibRegularService, R.id.ibGoodService, R.id.ibExcellentService})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ibRegularService:
+                percentage = REGULAR_TIP_PERCENTAGE;
+                break;
+            case R.id.ibGoodService:
+                percentage = DEFAULT_TIP_PERCENTAGE;
+                break;
+            case R.id.ibExcellentService:
+                percentage = EXCELLENT_TIP_PERCENTAGE;
+                break;
+        }
+    }
+}
